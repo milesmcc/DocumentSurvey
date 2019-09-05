2a9d26c680cb81913a6af24267292fd437227801
R. Miles McCain <t460-key@sendmiles.email>
2019-09-05 19:21:41 -0400

    Prepare for Heroku

--- DocumentSurvey/DocumentSurvey/settings.py
+++ DocumentSurvey/DocumentSurvey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oqp(ow3zl!%0g811=#)9ck)o76prn&ws^i$q^5^0-@w=0b^9ro'
+SECRET_KEY = os.environ.get("SECRET_KEY", "secretkey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: cd DocumentSurvey && gunicorn DocumentSurvey.wsgi --capture-output
+release: cd DocumentSurvey && python3 manage.py migrate
